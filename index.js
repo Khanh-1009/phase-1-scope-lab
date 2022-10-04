a36fe374d747ebdc7aaf0b7d0f3811f034e55e9f
Khanh Tran <katetrannbkhanh@gmail.com>
2022-10-04 16:39:28 -0700

    commit tree a82d673f9de84f730e40127a28f037146ae3701f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'client';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'customer'; 
+}
+
+/*var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+
+const leastFavoriteCustomer = 'Melissa';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Antar';
+    
+}*/
